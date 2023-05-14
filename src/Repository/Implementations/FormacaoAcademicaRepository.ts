423ebb469cb8604d8e54d169bb59f5e7421d1d17
Tales Xavier <talesxavier1@hotmail.com>
2023-05-14 15:39:45 -0300

    Ajustes na nomenclatura das funções.

--- src/Repository/Implementations/FormacaoAcademicaRepository.ts
+++ src/Repository/Implementations/FormacaoAcademicaRepository.ts
@@ -4,13 +4,13 @@ import IFormacaoAcademicaRepository from "../Interfaces/IFormacaoAcademicaReposi
 
 export default class FormacaoAcademicaRepository implements IFormacaoAcademicaRepository {
 
-    addAreaOfSpecialization = async (userToken: string, formacaoAcademica: FormacaoModel) => {
+    addAcademicBackground = async (userToken: string, formacaoAcademica: FormacaoModel) => {
         let url = `${import.meta.env.VITE_REACT_APP_API}/academicBackground/addAcademicBackground`;
         let response: any = await RequestModel().request(url, "POST", formacaoAcademica, { "userToken": userToken }, true);
         return response?.oparationStatus == 0;
     }
 
-    countAreaOfSpecialization = async (userToken: string) => {
+    countAcademicBackgrounds = async (userToken: string) => {
         let url = `${import.meta.env.VITE_REACT_APP_API}/academicBackground/countAcademicBackgrounds`;
         let response: any = await RequestModel().request(url, "GET", null, { "userToken": userToken }, true);
         if (response?.oparationStatus == 0) {
@@ -19,7 +19,7 @@ export default class FormacaoAcademicaRepository implements IFormacaoAcademicaRe
         return 0;
     }
 
-    getAreaOfSpecializationById = async (userToken: string, codigo: string) => {
+    getAcademicBackgroundById = async (userToken: string, codigo: string) => {
         let url = `${import.meta.env.VITE_REACT_APP_API}/academicBackground/getAcademicBackgroundById?codigo=${codigo}`;
         let response: any = await RequestModel().request(url, "GET", null, { "userToken": userToken }, true);
         if (response?.oparationStatus == 0) {
@@ -28,7 +28,7 @@ export default class FormacaoAcademicaRepository implements IFormacaoAcademicaRe
         return null;
     }
 
-    getAreasOfSpecialization = async (userToken: string, skip: number, take: number) => {
+    getAcademicBackgrounds = async (userToken: string, skip: number, take: number) => {
         let url = `${import.meta.env.VITE_REACT_APP_API}/academicBackground/getAcademicBackgrounds?skip=${skip}&take=${take}`;
         let response: any = await RequestModel().request(url, "GET", null, { "userToken": userToken }, true);
         if (response?.oparationStatus == 0) {
@@ -37,13 +37,13 @@ export default class FormacaoAcademicaRepository implements IFormacaoAcademicaRe
         return [];
     }
 
-    logicalDeleteAreaOfSpecialization = async (userToken: string, codigo: string) => {
+    logicalDeleteAcademicBackground = async (userToken: string, codigo: string) => {
         let url = `${import.meta.env.VITE_REACT_APP_API}/academicBackground/logicalDeleteAcademicBackground?codigos=${codigo}`;
         let response: any = await RequestModel().request(url, "POST", null, { "userToken": userToken }, true);
         return response?.oparationStatus == 0;
     }
 
-    modifyAreaOfSpecialization = async (userToken: string, formacaoAcademica: FormacaoModel) => {
+    modifyAcademicBackground = async (userToken: string, formacaoAcademica: FormacaoModel) => {
         let url = `${import.meta.env.VITE_REACT_APP_API}/academicBackground/modifyAcademicBackground`;
         let response: any = await RequestModel().request(url, "POST", formacaoAcademica, { "userToken": userToken }, true);
         return response?.oparationStatus == 0;
