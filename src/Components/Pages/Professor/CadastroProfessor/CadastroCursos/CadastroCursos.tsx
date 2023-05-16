4c008b5765b1ae8f1f903910c4a35b6604b880e2
Tales Xavier <talesxavier1@hotmail.com>
2023-05-15 21:55:54 -0300

    Criação dos componentes do cadastro de curso.

--- src/Components/Pages/Professor/CadastroProfessor/CadastroCursos/CadastroCursos.tsx
+++ src/Components/Pages/Professor/CadastroProfessor/CadastroCursos/CadastroCursos.tsx
@@ -2,6 +2,7 @@ import styles from "./CadastroCursos.module.css"
 import ButtonComponent from "../../../../Components/Button/ButtonComponent"
 import { useState } from "react";
 import ModalComponent from "../../../../Components/Modal/ModalComponent";
+import TextFieldComponent from "../../../../Components/TextField/TextFieldComponent";
 
 interface ICadastroCursos {
     id: string
@@ -33,8 +34,7 @@ const CadastroCursos = (props: ICadastroCursos) => {
                     btnSaveAction={() => { }}
                 />
                 </>
-                <><ButtonComponent
-                    value='Adicionar'
+                <><ButtonComponent value='Adicionar'
                     variant='outlined'
                     style={{
                         color: '#222834',
@@ -43,8 +43,7 @@ const CadastroCursos = (props: ICadastroCursos) => {
                     onClick={callBackcloseOpenModal}
                 />
                 </>
-                <><ButtonComponent
-                    value='Editar'
+                <><ButtonComponent value='Editar'
                     variant='outlined'
                     style={{
                         color: '#222834',
@@ -54,8 +53,7 @@ const CadastroCursos = (props: ICadastroCursos) => {
                 // disabled={selectedRows.length != 1}
                 />
                 </>
-                <><ButtonComponent
-                    value='Excluir'
+                <><ButtonComponent value='Excluir'
                     variant='outlined'
                     style={{
                         color: '#222834',
@@ -72,7 +70,73 @@ const CadastroCursos = (props: ICadastroCursos) => {
 
 const Content = () => {
     return (
-        <h1>CONTENT</h1>
+        <div className={styles["content_container"]}>
+            <><TextFieldComponent label='Código'
+                readonly
+                // inputRef={refsMap.get("codigo")}
+                // value={pessoaContext?.pessoa?.codigo ?? ""}
+                id={styles["codigo"]}
+                sx={{ width: "330px" }}
+            />
+            </>
+            <><TextFieldComponent label='Nome do Curso'
+                // inputRef={refsMap.get("codigo")}
+                // value={pessoaContext?.pessoa?.codigo ?? ""}
+                id={styles["nome_do_curso"]}
+                sx={{ width: "100%" }}
+            />
+            </>
+            <><TextFieldComponent label='Instituição de Ensino'
+                // inputRef={refsMap.get("codigo")}
+                // value={pessoaContext?.pessoa?.codigo ?? ""}
+                id={styles["instituicao_de_ensino"]}
+                sx={{ width: "100%" }}
+            />
+            </>
+            <><TextFieldComponent label='Carga Horária'
+                // inputRef={refsMap.get("codigo")}
+                // value={pessoaContext?.pessoa?.codigo ?? ""}
+                id={styles["carga_horaria"]}
+                sx={{ width: "100%" }}
+            />
+            </>
+            <><TextFieldComponent label='Data de Início'
+                // inputRef={refsMap.get("codigo")}
+                // value={pessoaContext?.pessoa?.codigo ?? ""}
+                id={styles["data_de_inicio"]}
+                sx={{ width: "100%" }}
+            />
+            </>
+            <><TextFieldComponent label='Data de Fim'
+                // inputRef={refsMap.get("codigo")}
+                // value={pessoaContext?.pessoa?.codigo ?? ""}
+                id={styles["data_de_fim"]}
+                sx={{ width: "100%" }}
+            />
+            </>
+            <><TextFieldComponent label='Modalidade do Curso'
+                // inputRef={refsMap.get("codigo")}
+                // value={pessoaContext?.pessoa?.codigo ?? ""}
+                id={styles["modalidade_do_curso"]}
+                sx={{ width: "100%" }}
+            />
+            </>
+            <><TextFieldComponent label='Investimento Financeiro'
+                // inputRef={refsMap.get("codigo")}
+                // value={pessoaContext?.pessoa?.codigo ?? ""}
+                id={styles["investimento_financeiro"]}
+                sx={{ width: "100%" }}
+            />
+            </>
+            <><TextFieldComponent label='Descrição do Curso'
+                // inputRef={refsMap.get("codigo")}
+                // value={pessoaContext?.pessoa?.codigo ?? ""}
+                id={styles["descricao_do_curso"]}
+                sx={{ width: "100%" }}
+                multiline={{ maxRows: 4, rows: 4 }}
+            />
+            </>
+        </div>
     )
 }
 
