1c09994aa4db1dd556b1e2bb14494d9a1fd9443c
Tales Xavier <talesxavier1@hotmail.com>
2023-05-24 21:35:47 -0300

    Alteração. Aplicação do parâmetro query no métodos get e count.

--- src/Repository/Implementations/CargoRepository.ts
+++ src/Repository/Implementations/CargoRepository.ts
@@ -6,22 +6,20 @@ import ICargoRepository from "../Interfaces/ICargoRepository";
 
 export default class CargoRepository implements ICargoRepository {
 
-
     addPosition = async (userToken: string, cargo: CargosModel) => {
         let url = `${import.meta.env.VITE_REACT_APP_API}/position/addPosition`;
         let response: any = await RequestModel().request(url, "POST", cargo, { "userToken": userToken }, true);
         return response?.oparationStatus == 0;
     };
 
-    countPositions = async (userToken: string) => {
-        let url = `${import.meta.env.VITE_REACT_APP_API}/position/countPositions`;
+    countPositions = async (userToken: string, query?: string) => {
+        let url = `${import.meta.env.VITE_REACT_APP_API}/position/countPositions{0}`.replace("{0}", query ? `?query=${query}` : "");
         let response: any = await RequestModel().request(url, "GET", null, { "userToken": userToken }, true);
         if (response?.oparationStatus == 0) {
             return response.data as number;
         }
         return 0;
     };
-
 
     getPositionById = async (userToken: string, codigo: string) => {
         let url = `${import.meta.env.VITE_REACT_APP_API}/position/getPositionById?codigo=${codigo}`;
@@ -32,9 +30,11 @@ export default class CargoRepository implements ICargoRepository {
         return null;
     };
 
-
-    getPositions = async (userToken: string, skip: number, take: number) => {
-        let url = `${import.meta.env.VITE_REACT_APP_API}/position/getPositions?skip=${skip}&take=${take}`;
+    getPositions = async (userToken: string, skip: number, take: number, query?: string) => {
+        let url = `${import.meta.env.VITE_REACT_APP_API}/position/getPositions?skip={0}&take={1}{2}`
+            .replace("{0}", skip.toString())
+            .replace("{1}", take.toString())
+            .replace("{2}", query ? `&query=${query}` : "");
         let response: any = await RequestModel().request(url, "GET", null, { "userToken": userToken }, true);
         if (response?.oparationStatus == 0) {
             return response.data as CargosModel[];
@@ -42,13 +42,11 @@ export default class CargoRepository implements ICargoRepository {
         return [];
     };
 
-
     logicalDeletePosition = async (userToken: string, codigo: string) => {
         let url = `${import.meta.env.VITE_REACT_APP_API}/position/logicalDeletePosition?codigos=${codigo}`;
         let response: any = await RequestModel().request(url, "POST", null, { "userToken": userToken }, true);
         return response?.oparationStatus == 0;
     };
-
 
     modifyPosition = async (userToken: string, cargo: CargosModel) => {
         let url = `${import.meta.env.VITE_REACT_APP_API}/position/modifyPosition`;
--- src/Repository/Interfaces/ICargoRepository.ts
+++ src/Repository/Interfaces/ICargoRepository.ts
@@ -3,9 +3,9 @@ import CargosModel from "../../Models/Objects/CargosModel";
 export default interface ICargoRepository {
 
     addPosition: (userToken: string, cargo: CargosModel) => Promise<boolean>;
-    countPositions: (userToken: string) => Promise<number>;
+    countPositions: (userToken: string, query?: string) => Promise<number>;
     getPositionById: (userToken: string, codigo: string) => Promise<CargosModel | null>;
-    getPositions: (userToken: string, skip: number, take: number) => Promise<CargosModel[]>;
+    getPositions: (userToken: string, skip: number, take: number, query?: string) => Promise<CargosModel[]>;
     logicalDeletePosition: (userToken: string, codigo: string) => Promise<boolean>;
     modifyPosition: (userToken: string, cargo: CargosModel) => Promise<boolean>;
 
