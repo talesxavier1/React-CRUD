ce0a17fc89e3847df84788be4d3d3ae97d05dadc
Tales Xavier <talesxavier1@hotmail.com>
2023-05-25 23:47:33 -0300

    Ajuste Implementação dos métodos de count e get por query.

--- src/Repository/Implementations/CargoRepository.ts
+++ src/Repository/Implementations/CargoRepository.ts
@@ -12,14 +12,23 @@ export default class CargoRepository implements ICargoRepository {
         return response?.oparationStatus == 0;
     };
 
-    countPositions = async (userToken: string, query?: string) => {
-        let url = `${import.meta.env.VITE_REACT_APP_API}/position/countPositions{0}`.replace("{0}", query ? `?query=${query}` : "");
+    countPositions = async (userToken: string) => {
+        let url = `${import.meta.env.VITE_REACT_APP_API}/position/countPositions`;
         let response: any = await RequestModel().request(url, "GET", null, { "userToken": userToken }, true);
         if (response?.oparationStatus == 0) {
             return response.data as number;
         }
         return 0;
     };
+
+    countPositionsByQuery = async (userToken: string, query: string) => {
+        let url = `${import.meta.env.VITE_REACT_APP_API}/position/countPositionsByQuery`;
+        let response: any = await RequestModel().request(url, "POST", query, { "userToken": userToken }, true);
+        if (response?.oparationStatus == 0) {
+            return response.data as number;
+        }
+        return 0;
+    }
 
     getPositionById = async (userToken: string, codigo: string) => {
         let url = `${import.meta.env.VITE_REACT_APP_API}/position/getPositionById?codigo=${codigo}`;
@@ -30,17 +39,27 @@ export default class CargoRepository implements ICargoRepository {
         return null;
     };
 
-    getPositions = async (userToken: string, skip: number, take: number, query?: string) => {
-        let url = `${import.meta.env.VITE_REACT_APP_API}/position/getPositions?skip={0}&take={1}{2}`
+    getPositions = async (userToken: string, skip: number, take: number) => {
+        let url = `${import.meta.env.VITE_REACT_APP_API}/position/getPositions?skip={0}&take={1}`
             .replace("{0}", skip.toString())
-            .replace("{1}", take.toString())
-            .replace("{2}", query ? `&query=${query}` : "");
+            .replace("{1}", take.toString());
         let response: any = await RequestModel().request(url, "GET", null, { "userToken": userToken }, true);
         if (response?.oparationStatus == 0) {
             return response.data as CargosModel[];
         }
         return [];
     };
+
+    getPositionsByQuery = async (userToken: string, skip: number, take: number, query: string) => {
+        let url = `${import.meta.env.VITE_REACT_APP_API}/position/getPositionsByQuery?skip={0}&take={1}`
+            .replace("{0}", skip.toString())
+            .replace("{1}", take.toString());
+        let response: any = await RequestModel().request(url, "POST", query, { "userToken": userToken }, true);
+        if (response?.oparationStatus == 0) {
+            return response.data as CargosModel[];
+        }
+        return [];
+    }
 
     logicalDeletePosition = async (userToken: string, codigo: string) => {
         let url = `${import.meta.env.VITE_REACT_APP_API}/position/logicalDeletePosition?codigos=${codigo}`;
--- src/Repository/Interfaces/IAreaAtuacaoRepository.ts
+++ src/Repository/Interfaces/IAreaAtuacaoRepository.ts
@@ -2,9 +2,11 @@ import AreaAtuacaoModel from "../../Models/Objects/AreaAtuacaoModel";
 
 export default interface IAreaAtuacaoRepository {
     addAreaOfSpecialization: (userToken: string, areaAtuacao: AreaAtuacaoModel) => Promise<boolean>;
-    countAreaOfSpecialization: (userToken: string, query?: string) => Promise<number>;
+    countAreaOfSpecialization: (userToken: string) => Promise<number>;
+    countAreaOfSpecializationByQuery: (userToken: string, query: string) => Promise<number>;
     getAreaOfSpecializationById: (userToken: string, codigo: string) => Promise<AreaAtuacaoModel | null>;
-    getAreasOfSpecialization: (userToken: string, skip: number, take: number, query?: string) => Promise<AreaAtuacaoModel[]>;
+    getAreasOfSpecialization: (userToken: string, skip: number, take: number) => Promise<AreaAtuacaoModel[]>;
+    getAreasOfSpecializationByQuery: (userToken: string, skip: number, take: number, query: string) => Promise<AreaAtuacaoModel[]>;
     logicalDeleteAreaOfSpecialization: (userToken: string, codigo: string) => Promise<boolean>;
     modifyAreaOfSpecialization: (userToken: string, areaAtuacao: AreaAtuacaoModel) => Promise<boolean>;
 }
