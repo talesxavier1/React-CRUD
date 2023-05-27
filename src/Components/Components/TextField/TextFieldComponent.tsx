b3867709fec567fcb8ac1c892cc75dd65195f585
Tales Xavier <talesxavier1@hotmail.com>
2023-05-27 20:24:24 -0300

    Melhoria Adição da valores numéricos e monetários no componente TextFieldComponent.

--- src/Components/Components/TextField/TextFieldComponent.tsx
+++ src/Components/Components/TextField/TextFieldComponent.tsx
@@ -1,4 +1,4 @@
-import { TextField } from '@mui/material';
+import { InputAdornment, TextField } from '@mui/material';
 import { Theme } from '@mui/material/styles/';
 import { SxProps } from '@mui/system/styleFunctionSx';
 import { mask } from "../../../utils/mask";
@@ -17,6 +17,8 @@ interface props {
     multiline?: {
         rows: number
     }
+    type?: "number" | "text" | "monetary"
+    monetaryType?: string
 }
 
 const TextFieldComponent = (props: props) => {
@@ -28,19 +30,36 @@ const TextFieldComponent = (props: props) => {
     return (
         <div id={props.id} className='textFieldComponet-bdc8183e'>
             <TextField
+
                 inputRef={props.inputRef}
                 sx={props.sx}
+                type={(() => {
+                    if (['number', "monetary"].includes(props.type ?? "")) {
+                        return "number";
+                    }
+                    return "text";
+                })()}
                 label={props.label}
                 value={(() => {
-                    if (!textFieldValue) { return "" }
+                    if (!textFieldValue) { return ['number', "monetary"].includes(props.type ?? "") ? 0 : "" }
                     if (props.mask) {
                         return new mask({ mask: props.mask, type: 'number', value: textFieldValue }).applyMask();
                     }
                     return textFieldValue
                 })()}
                 onChange={(e) => {
+                    if (['number', "monetary"].includes(props.type ?? "")) {
+                        setTextFieldValue(Number(e.currentTarget.value));
+                    }
                     setTextFieldValue(e.currentTarget.value);
                 }}
+                InputProps={(() => {
+                    if (props.type == "monetary") {
+                        return {
+                            startAdornment: <InputAdornment position="start">{props.monetaryType ? props.monetaryType : "$"}</InputAdornment>
+                        }
+                    }
+                })()}
                 disabled={props.readonly}
                 size='small'
                 multiline={props.multiline ? true : false}
