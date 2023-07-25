f19fd9b2d4aa7ce9cfa0c3c2ebc459b0d94f4901
Tales Xavier <talesxavier1@hotmail.com>
2023-07-25 19:57:50 -0300

    Criação. Criação do compoente AreaConhecimento.

---
+++ src/Components/Pages/Cadastros Gerais/Turma/Grade Curricular/Área de Conhecimento/AreaConhecimento.tsx
@@ -0,0 +1,14 @@
+import style from "./AreaConhecimento.module.css"
+
+
+const AreaConhecimento = () => {
+
+    return (
+        <div>
+            AreaConhecimento
+        </div>
+    )
+
+}
+
+export default AreaConhecimento;
