0d7f080964c453363cd0a7f05b05a029e6cc32b3
Tales Xavier <talesxavier1@hotmail.com>
2023-07-25 19:56:57 -0300

    Criação. Criação do componente ComponentesCurriculares.

---
+++ src/Components/Pages/Cadastros Gerais/Turma/Grade Curricular/Componentes Curriculares/ComponentesCurriculares.tsx
@@ -0,0 +1,16 @@
+import style from "./ComponentesCurriculares.module.css"
+
+
+const ComponentesCurriculares = () => {
+
+    return (
+        <div>
+            ComponentesCurriculares
+        </div>
+    )
+
+}
+
+export default ComponentesCurriculares;
+
+
