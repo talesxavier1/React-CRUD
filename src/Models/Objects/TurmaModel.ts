cbc3ea2baaa678bfce4247d2355a5ac497e51164
Tales Xavier <talesxavier1@hotmail.com>
2023-07-28 23:42:03 -0300

    Criação. Criação do Model TurmaModel.

---
+++ src/Models/Objects/TurmaModel.ts
@@ -0,0 +1,17 @@
+import ITurmaModel from "../Interfaces/ITurmaModel";
+
+export default class TurmaModel implements ITurmaModel {
+    "codigo"!: string
+    "classCode"!: string
+    "schoolYear"!: number
+    "classStatus"!: string
+    "gradeLevel"!: string
+    "classShift"!: string
+    "responsibleTeacher"!: string
+    "responsibleTeacherID"!: string
+    "observations"!: string
+    "classStartDate"!: string
+    "classEndtDate"!: string
+    "totalWorkload"!: number
+    "supplementaryWorkload"!: number
+}
