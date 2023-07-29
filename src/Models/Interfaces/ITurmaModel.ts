17d7589a336c9870a71d349357a7f1eb079d3d48
Tales Xavier <talesxavier1@hotmail.com>
2023-07-28 23:39:22 -0300

    Criação. Criação da interface ITurmaModel.

---
+++ src/Models/Interfaces/ITurmaModel.ts
@@ -0,0 +1,15 @@
+export default interface ITurmaModel {
+    "codigo": string
+    "classCode": string
+    "schoolYear": number,
+    "classStatus": string
+    "gradeLevel": string
+    "classShift": string
+    "responsibleTeacher": string
+    "responsibleTeacherID": string
+    "observations": string
+    "classStartDate": string
+    "classEndtDate": string
+    "totalWorkload": number
+    "supplementaryWorkload": number
+}
