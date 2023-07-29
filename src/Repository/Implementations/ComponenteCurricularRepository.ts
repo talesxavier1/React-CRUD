23ca8fc6c560d6e2d8abde3da22ee9d2335d5f5a
Tales Xavier <talesxavier1@hotmail.com>
2023-07-28 23:43:31 -0300

    Ajuste. Alteração no nome de TurmaComponenteCurricularRepository para ComponenteCurricularRepository.

--- src/Repository/Implementations/ComponenteCurricularRepository.ts
+++ src/Repository/Implementations/ComponenteCurricularRepository.ts
@@ -1,18 +1,17 @@
-import AreaAtuacaoModel from "../../Models/Objects/AreaAtuacaoModel";
-import { TurmaComponenteCurricularModel } from "../../Models/Objects/TurmaComponenteCurricularModel";
+import { ComponenteCurricularModel } from "../../Models/Objects/ComponenteCurricularModel";
 import { RequestModel } from "../../utils/Request";
-import ITurmaComponenteCurricularRepository from "../Interfaces/ITurmaComponenteCurricularRepository";
+import IComponenteCurricularRepository from "../Interfaces/IComponenteCurricularRepository";
 
-export class TurmaComponenteCurricularRepository implements ITurmaComponenteCurricularRepository {
+export class ComponenteCurricularRepository implements IComponenteCurricularRepository {
 
-    addCurricularComponent = async (userToken: string, componenteCurricular: TurmaComponenteCurricularModel) => {
-        let url = `${import.meta.env.VITE_REACT_APP_API}/ClassCurricularComponent/addCurricularComponent`;
+    addCurricularComponent = async (userToken: string, componenteCurricular: ComponenteCurricularModel) => {
+        let url = `${import.meta.env.VITE_REACT_APP_API}/CurricularComponent/addCurricularComponent`;
         let response: any = await RequestModel().request(url, "POST", componenteCurricular, { "userToken": userToken }, true);
         return response?.oparationStatus == 0;
     };
 
     countCurricularComponents = async (userToken: string) => {
-        let url = `${import.meta.env.VITE_REACT_APP_API}/ClassCurricularComponent/countCurricularComponents`;
+        let url = `${import.meta.env.VITE_REACT_APP_API}/CurricularComponent/countCurricularComponents`;
         let response: any = await RequestModel().request(url, "GET", null, { "userToken": userToken }, true);
         if (response?.oparationStatus == 0) {
             return (response.data as number);
@@ -21,7 +20,7 @@ export class TurmaComponenteCurricularRepository implements ITurmaComponenteCurr
     };
 
     countCurricularComponentsByQuery = async (userToken: string, query: string) => {
-        let url = `${import.meta.env.VITE_REACT_APP_API}/ClassCurricularComponent/countCurricularComponentsByQuery`;
+        let url = `${import.meta.env.VITE_REACT_APP_API}/CurricularComponent/countCurricularComponentsByQuery`;
         let response: any = await RequestModel().request(url, "POST", query, { "userToken": userToken, "Content-Type": 'application/json' }, true);
         if (response?.oparationStatus == 0) {
             return (response.data as number);
@@ -30,41 +29,41 @@ export class TurmaComponenteCurricularRepository implements ITurmaComponenteCurr
     };
 
     getCurricularComponentById = async (userToken: string, codigo: string) => {
-        let url = `${import.meta.env.VITE_REACT_APP_API}/ClassCurricularComponent/getCurricularComponentById?codigo=${codigo}`;
+        let url = `${import.meta.env.VITE_REACT_APP_API}/CurricularComponent/getCurricularComponentById?codigo=${codigo}`;
         let response: any = await RequestModel().request(url, "GET", null, { "userToken": userToken }, true);
         if (response?.oparationStatus == 0) {
-            return (response.data as TurmaComponenteCurricularModel);
+            return (response.data as ComponenteCurricularModel);
         }
         return null;
     };
 
     getCurricularComponents = async (userToken: string, skip: number, take: number) => {
-        let url = `${import.meta.env.VITE_REACT_APP_API}/ClassCurricularComponent/getCurricularComponents?skip=${skip}&take=${take}`;
+        let url = `${import.meta.env.VITE_REACT_APP_API}/CurricularComponent/getCurricularComponents?skip=${skip}&take=${take}`;
         let response: any = await RequestModel().request(url, "GET", null, { "userToken": userToken }, true);
         if (response?.oparationStatus == 0) {
-            return (response.data as TurmaComponenteCurricularModel[]);
+            return (response.data as ComponenteCurricularModel[]);
         }
         return [];
     };
 
     getCurricularComponentsByQuery = async (userToken: string, skip: number, take: number, query: string) => {
-        let url = `${import.meta.env.VITE_REACT_APP_API}/ClassCurricularComponent/getCurricularComponentsByQuery?skip=${skip}&take=${take}`;
+        let url = `${import.meta.env.VITE_REACT_APP_API}/CurricularComponent/getCurricularComponentsByQuery?skip=${skip}&take=${take}`;
         let response: any = await RequestModel().request(url, "POST", query, { "userToken": userToken, "Content-Type": 'application/json' }, true);
         if (response?.oparationStatus == 0) {
-            return (response.data as TurmaComponenteCurricularModel[]);
+            return (response.data as ComponenteCurricularModel[]);
         }
         return [];
     };
 
     logicalDeleteCurricularComponent = async (userToken: string, codigo: string) => {
-        let url = `${import.meta.env.VITE_REACT_APP_API}/ClassCurricularComponent/logicalDeleteCurricularComponent?codigos=${codigo}`;
+        let url = `${import.meta.env.VITE_REACT_APP_API}/CurricularComponent/logicalDeleteCurricularComponent?codigos=${codigo}`;
         let response: any = await RequestModel().request(url, "POST", null, { "userToken": userToken }, true);
         return response?.oparationStatus == 0;
     };
 
-    modifyCurricularComponent = async (userToken: string, areaAtuacao: AreaAtuacaoModel) => {
-        let url = `${import.meta.env.VITE_REACT_APP_API}/ClassCurricularComponent/modifyCurricularComponent`;
-        let response: any = await RequestModel().request(url, "POST", areaAtuacao, { "userToken": userToken }, true);
+    modifyCurricularComponent = async (userToken: string, componenteCurricular: ComponenteCurricularModel) => {
+        let url = `${import.meta.env.VITE_REACT_APP_API}/CurricularComponent/modifyCurricularComponent`;
+        let response: any = await RequestModel().request(url, "POST", componenteCurricular, { "userToken": userToken }, true);
         return response?.oparationStatus == 0;
     };
 }
