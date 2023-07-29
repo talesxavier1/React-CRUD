da7be7633a31895c2fa055a67fa1163f8d2fa823
Tales Xavier <talesxavier1@hotmail.com>
2023-07-28 23:40:43 -0300

    Ajuste. Alteração no nome do componente de TurmaAreaConhecimentoModel para AreaConhecimentoModel.

--- src/Models/Objects/TurmaAreaConhecimento.ts
+++
@@ -1,6 +0,0 @@
-import ITurmaAreaConhecimentoModel from "../Interfaces/ITurmaAreaConhecimentoModel";
-
-export default class TurmaAreaConhecimentoModel implements ITurmaAreaConhecimentoModel {
-    codigo!: string;
-    area!: string;
-}
