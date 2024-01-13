1acd47c4a21d47cef53e951bc0ad74db1190d4cd
talesxavier1 <Talesxavier1@gmail.com>
2024-01-12 22:15:08 -0300

    s

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
 const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.Suspense>
     <QueryClientProvider client={queryClient}>
